dbff6a3a80a8d1bc98744ccfa3b00830f2adbda0
Boris <boris@felicons.com>
2024-11-30 21:12:56 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+// import { Core } from "panelkit-core";
+// import "panelkit-core/dist/panelkit-styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Core id="core" /> */}
+      test
+    </div>
+  );
+}
+
+export default App;
