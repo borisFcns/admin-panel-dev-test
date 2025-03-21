fefc61d2b5dbe91d368be1a84e78cdcbe16b6fd4
boris.k <boris.k@fxgt.com>
2025-03-21 13:44:19 +0100

    update App component to use environment variables for API and auth URLs; bump version to 0.1.2 and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import "./App.css";
 import { CoreWrapper } from "@borisfcns/panelkit-core";
 import "@borisfcns/panelkit-core/dist/panelkit-styles.css";
 
+const apiUrl = process.env.REACT_APP_API_URL;
+const authUrl = process.env.REACT_APP_AUTH_URL;
+
 function App() {
+    if (!apiUrl || !authUrl) {
+        throw new Error("API URL or AUTH URL not set");
+    }
+
   return (
     <div className="App">
         {/*@ts-ignore*/}
       <CoreWrapper id="core"
-            basename="admin-panel-dev-test"
-            urls={{
-                apiUrl: "https://manager-api.felicons.com:2096",
-                authUrl:  "https://manager-auth-api.felicons.com:2096",
-            }} />
+            basename="admin-panel-dev-test" urls={{apiUrl, authUrl}}
+      />
     </div>
   );
 }
